e164d621d8c9d28811832de921f20485bf25ca2d
Kaialogen <kaialogen@protonmail.com>
2022-12-01 13:35:30 +0000

    broke

--- js/order.js
+++ js/order.js
@@ -6,8 +6,7 @@ function storeData() {
       formData[inputId] = document.getElementById(inputId).value;
     })
     localStorage.setItem("paymentDetails", JSON.stringify(formData));
-
-    window.location.href='/invoice.html';
+    //window.location.href='/invoice.html';
   }
 
   const container = document.getElementById("payment-details");
@@ -43,7 +42,7 @@ function storeData() {
     var postcode_regex = /^(GIR[ ]?0AA|((AB|AL|B|BA|BB|BD|BH|BL|BN|BR|BS|BT|CA|CB|CF|CH|CM|CO|CR|CT|CV|CW|DA|DD|DE|DG|DH|DL|DN|DT|DY|E|EC|EH|EN|EX|FK|FY|G|GL|GY|GU|HA|HD|HG|HP|HR|HS|HU|HX|IG|IM|IP|IV|JE|KA|KT|KW|KY|L|LA|LD|LE|LL|LN|LS|LU|M|ME|MK|ML|N|NE|NG|NN|NP|NR|NW|OL|OX|PA|PE|PH|PL|PO|PR|RG|RH|RM|S|SA|SE|SG|SK|SL|SM|SN|SO|SP|SR|SS|ST|SW|SY|TA|TD|TF|TN|TQ|TR|TS|TW|UB|W|WA|WC|WD|WF|WN|WR|WS|WV|YO|ZE)(\d[\dA-Z]?[ ]?\d[ABD-HJLN-UW-Z]{2}))|BFPO[ ]?\d{1,4})$/;
     var postcodes = String(postcode_check).match(postcode_regex);
     var title_check = document.getElementById("title").value;
-    var first_check = document.getElementById("first").value;
+    var first_check = document.getElementById("fname").value;
     var last_check = document.getElementById("last").value;
     var payment_name_check = document.getElementById("cardholder").value;
     var check_card_number = document.getElementById("cardnumber").value;
