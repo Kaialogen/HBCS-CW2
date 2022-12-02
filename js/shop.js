bc9e9f89d3b793a340c00499083e6c5a54ed4015
Kaialogen <kaialogen@protonmail.com>
2022-12-02 13:53:48 +0000

    div adding

--- js/shop.js
+++ js/shop.js
@@ -39,6 +39,36 @@ function addToBasket(movieId) {
         movie.rentDays = additional;
     }
 
+    if (exists == 1){
+        basket[save.indexOf(movie.name)].rentDays += additional;
+        if (basket[save.indexOf(movie.name)].rentDays < 1){
+            alert("Item has been removed from the basket.");
+            basket.splice(save.indexOf(movie.name), 1);
+            save.splice(save.indexOf(movie.name), 1);
+        }
+        
+        else if (basket[save.indexOf(movie.name)].rentDays > 30){
+            if (ifalert == 0){
+                alert("Sorry you cannot rent a movie longer than 30 days.  Rent time has been changed to 30 days.");
+            }
+            basket[save.indexOf(movie.name)].rentDays = 30;
+            alert(movie.name + " has been added to the basket.\nYou have chosen to rent this movie for " + basket[save.indexOf(movie.name)].rentDays + " days.")
+        }
+        
+        else{
+            alert(movie.name + " has been added to the basket.\nYou have chosen to rent this movie for " + basket[save.indexOf(movie.name)].rentDays + " days.")
+        }
+    }
+    else
+    {
+        if (movie.rentDays < 1) {
+            movie.rentDays = 1;
+            alert("Sorry you cannot rent a movie for less than a day.  Rent time has been changed to 1 day.");
+            }
+        basket.push(movie);
+        alert(movie.name + " has been added to the basket.\nYou have chosen to rent this movie for " + basket[save.indexOf(movie.name)].rentDays + " days.")
+    }
+    
     basket.push(movie);
     localStorage.setItem("save", JSON.stringify(save));
     localStorage.setItem("basket", JSON.stringify(basket));
