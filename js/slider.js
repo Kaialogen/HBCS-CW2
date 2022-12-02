7b82af5ca522594f699456360ddb477171df6706
Kaialogen <kaialogen@protonmail.com>
2022-12-02 09:51:30 +0000

    slideshow changes

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,7 @@ let slideIndex = 0;
 
 //Function calls for automatic and manual slideshows -- Comment out the one you do not want to use;
 
-//showSlidesManual(slideIndex);
+showSlidesManual(slideIndex);
 showSlidesAutomatic();
 
 
@@ -20,17 +20,12 @@ function plusSlides(n) {
 function showSlidesManual(n) {
 let i;
 let slides = document.getElementsByClassName("mySlides");
-let dots = document.getElementsByClassName("dot");
-if (n > slides.length) {slideIndex = 1}
 if (n < 1) {slideIndex = slides.length}
 for (i = 0; i < slides.length; i++) {
 slides[i].style.display = "none";
 }
-for (i = 0; i < dots.length; i++) {
-dots[i].className = dots[i].className.replace(" active", "");
-}
+if (n > slides.length) {slideIndex = 1}
 slides[slideIndex-1].style.display = "block";
-dots[slideIndex-1].className += " active";
 }
 
 function showSlidesAutomatic() {
@@ -42,5 +37,5 @@ slides[i].style.display = "none";
 slideIndex++;
 if (slideIndex > slides.length) {slideIndex = 1}
 slides[slideIndex-1].style.display = "block";
-setTimeout(showSlidesAutomatic, 5000); // Change image every 5 seconds
+setTimeout(showSlidesAutomatic, 10000); // Change image every 10 seconds
 }
