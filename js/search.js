3d112e9e66a20dee121e87dc6b2715ac4ed58b10
Kaialogen <kaialogen@protonmail.com>
2022-12-03 19:02:48 +0000

    improved search algorithm

--- js/search.js
+++ js/search.js
@@ -1,57 +1,35 @@
 function submitted() {
-    Input = document.getElementById("query").value
-      Input = Input.toLowerCase()
-      FullURL = document.URL.split("/Order.html")
+  const input = document.getElementById("query").value.toLowerCase();
+  const baseURL = document.URL.split("http://localhost:8080")[0];
 
-      if (Input.includes("paw")){
-          window.open("http://localhost:8080/Movie.html?id=2")
-        }
-      else if (Input.includes("patrol")){
-          window.open("http://localhost:8080//Movie.html?id=2")
-      }
-      else if (Input.includes("demon")){
-          window.open("http://localhost:8080//Movie.html?id=7")
-      }
-      else if (Input.includes("shang")){
-        window.open("http://localhost:8080//Movie.html?id=1")
-      }
-      else if (Input.includes("legend")){
-        window.open("http://localhost:8080//Movie.html?id=1")
-      }
-      else if (Input.includes("dog")){
-        window.open("http://localhost:8080//Movie.html?id=8")
-      }
-      else if (Input.includes("power")){
-        window.open("http://localhost:8080//Movie.html?id=8")
-      }
-      else if (Input.includes("bat")){
-        window.open("http://localhost:8080//Movie.html?id=5")
-      }
-      else if (Input.includes("dark")){
-        window.open("http://localhost:8080//Movie.html?id=5")
-      }
-      else if (Input.includes("knight")){
-        window.open("http://localhost:8080//Movie.html?id=5")
-      }
-      else if (Input.includes("spider") && Input.includes("2")){
-        window.open("http://localhost:8080//Movie.html?id=9")
-      }
-      else if (Input.includes("spider") && Input.includes("3")){
-        window.open("http://localhost:8080//Movie.html?id=3")
-      }
-      else if (Input.includes("don't")){
-        window.open("http://localhost:8080//Movie.html?id=4")
-      }
-      else if (Input.includes("breathe")){
-        window.open("http://localhost:8080//Movie.html?id=4")
-      }
-      else if (Input.includes("mat")){
-        window.open("http://localhost:8080//Movie.html?id=6")
-      }
-      else if (Input.includes("shrek")){
-        window.open("http://localhost:8080/movie.html?id=11")
-      }
-      else {
-        alert("Sadly we couldn't identify that movie in our catalogue")
+  // Use an object to map movie keywords to their IDs
+  const movieMap = {
+      paw: 2,
+      patrol: 2,
+      demon: 7,
+      shang: 1,
+      legend: 1,
+      dog: 8,
+      power: 8,
+      bat: 5,
+      dark: 5,
+      knight: 5,
+      "spiderman 2": 9,
+      "spiderman 3": 3,
+      "don't": 4,
+      breathe: 4,
+      matrix: 6,
+      "the matrix": 6,
+      shrek: 11,
+      bee: 12,
+      movie: 12,
+  };
+
+  // Check if the input matches any keywords
+  const id = movieMap[input];
+  if (id) {
+      window.open(`${baseURL}/Movie.html?id=${id}`);
+  } else {
+      alert("We couldn't identify that movie in our catalogue");
   }
 }
