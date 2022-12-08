48deea266d39afdab7bf652572913644b8475e02
Kaialogen <kaialogen@protonmail.com>
2022-12-08 23:20:16 +0000

    another emergency fix

--- js/shop.js
+++ js/shop.js
@@ -53,7 +53,6 @@ function addToBasket(movieId) {
     }
 
     /* Splice Algorithm - breaks everything */
-    /*
     if (exists == 1){
         basket[save.indexOf(movie.name)].rentDays += additional;
         if (basket[save.indexOf(movie.name)].rentDays < 1){
@@ -61,17 +60,29 @@ function addToBasket(movieId) {
             basket.splice(save.indexOf(movie.name), 1);
             save.splice(save.indexOf(movie.name), 1);
         }
+
+        else if (basket[save.indexOf(movie.name)].rentDays > 30){
+            if (ifalert == 0){
+                alert("Sorry you cannot rent a movie longer than 30 days.  Rent time has been changed to 30 days.");
+            }
+            basket[save.indexOf(movie.name)].rentDays = 30;
+            alert(movie.name + " has been added to the basket.\nYou have chosen to rent this movie for " + basket[save.indexOf(movie.name)].rentDays + " days.")
+        }
+        
+        else{
+            alert(movie.name + " has been added to the basket.\nYou have chosen to rent this movie for " + basket[save.indexOf(movie.name)].rentDays + " days.")
+        }
     }
     else
     {
+        if (movie.rentDays < 1) {
+            movie.rentDays = 1;
+            alert("Sorry you cannot rent a movie for less than a day.  Rent time has been changed to 1 day.");
+            }
         basket.push(movie);
         alert(movie.name + " has been added to basket");
     }
-    */
 
-
-    basket.push(movie);
-    alert(movie.name + " has been added to basket");
     
     localStorage.setItem("save", JSON.stringify(save));
     localStorage.setItem("basket", JSON.stringify(basket));
